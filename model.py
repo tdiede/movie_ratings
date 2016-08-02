60a9524ca9605eedbadf2aaf9dd8059aa634babf
Hackbright Student <info@hackbrightacademy.com>
2016-08-01 18:00:54 -0700

    Part Two, up to Flask

--- model.py
+++ model.py
@@ -17,12 +17,55 @@ class User(db.Model):
 
     __tablename__ = "users"
 
+    def __repr__(self):
+        """Show info about the user."""
+
+        return "<User user_id=%s email=%s>" % (self.user_id, self.email)
+
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     email = db.Column(db.String(64), nullable=True)
     password = db.Column(db.String(64), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
+
+class Movie(db.Model):
+    """Movies to be rated."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    released_at = db.Column(db.DateTime, nullable=True)
+    imdb_url = db.Column(db.String(256), nullable=True)
+
+
+class Rating(db.Model):
+    """Ratings given by user."""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    score = db.Column(db.Integer)
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+
+    # Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>"
+        return s % (self.rating_id, self.movie_id, self.user_id,
+                    self.score)
 
 # Put your Movie and Rating model classes here.
 
--- seed.py
+++ seed.py
@@ -1,9 +1,8 @@
 """Utility file to seed ratings database from MovieLens data in seed_data/"""
 
 from sqlalchemy import func
-from model import User
-# from model import Rating
-# from model import Movie
+from model import User, Movie, Rating
+import datetime
 
 from model import connect_to_db, db
 from server import app
@@ -37,13 +36,67 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
+    print "Movies"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Movie.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        row = row.split("|")
+        row = row[:5]
+        movie_id, title, released_at, video_release_at, imdb_url = row
+
+        title = title[:-7]
+        # title = title.split(" (")
+        # title = title[0]
+
+        if released_at:
+            released_at = datetime.datetime.strptime(released_at, "%d-%b-%Y")
+        else:
+            released_at = None
+
+        movie = Movie(movie_id=movie_id,
+                    title=title,
+                    released_at=released_at,
+                    imdb_url=imdb_url)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(movie)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
 
 def load_ratings():
     """Load ratings from u.data into database."""
 
+    print "Ratings"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Rating.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        user_id, movie_id, score, timestamp = row.split("\t")
+
+        rating = Rating(user_id=user_id,
+                    movie_id=movie_id,
+                    score=score)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(rating)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
 
 def set_val_user_id():
-    """Set value for the next user_id after seeding database"""
+    """Set value for the next user_id after seeding database."""
 
     # Get the Max user_id in the database
     result = db.session.query(func.max(User.user_id)).one()
@@ -51,6 +104,32 @@ def set_val_user_id():
 
     # Set the value for the next user_id to be max_id + 1
     query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+def set_val_movie_id():
+    """Set value for the next movie_id after seeding database."""
+
+    # Get the Max movie_id in the database
+    result = db.session.query(func.max(Movie.movie_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next movie_id to be max_id + 1
+    query = "SELECT setval('movies_movie_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+def set_val_rating_id():
+    """Set value for the next rating_id after seeding database."""
+
+    # Get the Max rating_id in the database
+    result = db.session.query(func.max(Rating.rating_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next rating_id to be max_id + 1
+    query = "SELECT setval('ratings_rating_id_seq', :new_id)"
     db.session.execute(query, {'new_id': max_id + 1})
     db.session.commit()
 
@@ -66,3 +145,5 @@ if __name__ == "__main__":
     load_movies()
     load_ratings()
     set_val_user_id()
+    set_val_movie_id()
+    set_val_rating_id()
